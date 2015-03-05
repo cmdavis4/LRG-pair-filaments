74e2ca7a8be87d882366fe1a4b72232a84ee3696
cmdavis4 <charles.m.davis.iv@gmail.com>
2015-03-05 16:00:51 -0500

    Got it to run, optimized to make running time reasonable

--- neighbors_match.py
+++ neighbors_match.py
@@ -3,15 +3,33 @@ import healpy as hp
 import bisect as bs
 import spherical_geo as sg
 import cosmology_functions as cf
+from healpy_settings import healpy_params as hpparam
 
-n_path = ''
-p_path = ''
-out_dir = ''
-out_name = ''
+n_path = '/home/chadavis/catalog_creation/LRG-pair-filaments/neighbors_with_pix.csv'
+p_path = '/home/chadavis/catalog_creation/LRG-pair-filaments/pairs_with_pix.csv'
+out_dir = '/home/chadavis/catalog_creation/LRG-pair-filaments/'
+out_name = 'matches.csv'
 
-neighbors = np.genfromtxt(n_path, delimiter=',', dtype=str, names=True)
-pairs = np.genfromtxt(p_path, delimiter=',', dtype=str, names=True)
-NSIDE = 16
+print 'Reading neighbors...'
+neighbors = np.genfromtxt(n_path, delimiter=',', dtype=str)
+print 'Reading pairs...'
+pairs = np.genfromtxt(p_path, delimiter=',', dtype=str)
+
+n_header = neighbors[0]
+neighbors = neighbors[1:,]
+p_header = pairs[0]
+pairs = pairs[1:,]
+
+nobjid = 0
+pobjid = 7
+lrgid = 0
+ra_mid = 1
+dec_mid = 2
+ra_gal = 11
+dec_gal = 12
+z = 13
+
+NSIDE = hpparam['NSIDE']
 
 '''
 Steps for one pair:
@@ -26,27 +44,49 @@ Steps for one pair:
 matches = np.array([])
 
 def matchPix(p):
-    lind = lambda x: bs.bisect_left(pairs['pix'].astype(int), x)
-    rind = lambda x: bs.bisect_right(pairs['pix'].astype(int), x)
-    return zip(map(lind, p), map(rind, p))
+    lind = lambda x: bs.bisect_left(neighbors[:,-1].astype(int), x)
+    rind = lambda x: bs.bisect_right(neighbors[:,-1].astype(int), x)
+    get_inds = lambda x: range(lind(x), rind(x))
+    inds = np.concatenate(map(get_inds, p))
+    return np.unique(inds).astype(int)
 
 def distance(ra_p, dec_p, ra_n, dec_n, z_n):
-    angle = sg.calc_distance(ra_p, dec_p, ra_n, dec_n)
+    angle = sg.calc_distance((ra_p, dec_p), zip(ra_n, dec_n))
     adds = map(cf.angDiamDistSingle, z_n)
     return np.multiply(angle, adds)
 
-for n in neighbors:
-    pnum = float(n['pix'])
+
+pix_nums = neighbors[:,-1].astype(int)
+print 'Creating pixel dictionary...'
+pixDict = {}
+for j in range(len(pix_nums)):
+    pixDict.setdefault(pix_nums[j],[]).append(j)
+    
+matches = []
+
+print 'Matching...'
+for p in pairs:
+    pnum = int(p[-1])
     near_pix = hp.get_all_neighbours(NSIDE, pnum)
-    inds = matchPix(p)
+    print pnum
+    inds = []
+    for j in near_pix:
+        try:
+            inds.append(pixDict[int(j)])
+        except KeyError:
+            pass
+    inds = np.concatenate(inds).astype(int)
     candidates = neighbors[inds]
-    distances = distance(n['ra_mid'].astype(float), n['dec_mid'].astype(float),
-                         candidates['ra_gal'].astype(float),
-                         candidates['dec_gal'].astype(float),
-                         candidates['z'].astype(float))
+    print np.shape(candidates)
+    distances = distance(p[ra_mid].astype(float), p[dec_mid].astype(float),
+                         candidates[:,ra_gal].astype(float),
+                         candidates[:,dec_gal].astype(float),
+                         candidates[:,z].astype(float))
     inbounds = neighbors[inds[distances <= 15]]
-    inbounds = hstack((inbounds, [n['id']] * len(inbounds)))
-    matches = vstack((matches, inbounds))
+    inbounds = inbounds[np.where(inbounds[:,nobjid] != p[pobjid])[0]]
+    idnum_arr = np.array([p[lrgid]] * np.shape(inbounds)[0]).reshape(np.shape(inbounds)[0], 1)
+    inbounds = np.hstack((inbounds, idnum_arr))
+    matches.append(inbounds)
+    
     
 np.savetxt(out_dir + out_name, matches, fmt='%s', delimiter=',')
-    
